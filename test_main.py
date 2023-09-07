c24129f3a5a030a8813f12c5d5dc2c30e28cbecb
Liam <ml646@duke.edu>
2023-09-06 20:56:11 -0400

    test func add count&mean

--- test_main.py
+++ test_main.py
@@ -3,6 +3,6 @@ from main import describeData as f
 
 def test_func():
     df = f("https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv")
-    
-    
-    print(df)
+
+    assert df.iloc[:2,0].values[0]==128 # count
+    assert df.iloc[:2,0].values[1]==38.8203125 # mean
