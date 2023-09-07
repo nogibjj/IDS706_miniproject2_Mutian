badd0f4bba0455e22ff4ca1b4a916598b2d77d23
Liam <ml646@duke.edu>
2023-09-06 20:43:05 -0400

    update test_main

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ from main import describeData as f
 
 
 def test_func():
-    df = describeData("https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv")
+    df = f("https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv")
     
     
     print(df)
