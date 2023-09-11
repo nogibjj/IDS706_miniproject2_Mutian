9b5b17a1c0ef570df4e358b836778ad1c33b6be4
Liam <ml646@duke.edu>
2023-09-11 12:30:20 -0400

    update scatter

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def plotData(df):
     y = df['Age']
     color= np.array([np.random.randint(0,100) for _ in range(df.shape[0])])
     plt.scatter(x,y,c=color,cmap='viridis',s=100)
+    plt.xlabel("year")
+    plt.ylabel("age")
     plt.colorbar()
     plt.show()
     plt.savefig("./scatterfig.png")
