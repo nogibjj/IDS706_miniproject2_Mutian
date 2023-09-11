29ce45b78e1d74520063ab31c1b82e6b8f3f81e3
Liam <ml646@duke.edu>
2023-09-11 11:55:09 -0400

    descriptive data

--- test_main.py
+++ test_main.py
@@ -1,8 +1,12 @@
-from main import describeData as f
-
+from main import *
 
 def test_func():
-    df = f("https://people.sc.fsu.edu/~jburkardt/data/csv/cities.csv")
-
-    assert df.iloc[:2,0].values[0]==128 # count
-    assert df.iloc[:2,0].values[1]==38.8203125 # mean
+    df = loadDf("https://people.sc.fsu.edu/~jburkardt/data/csv/oscar_age_female.csv")
+    descriptive_df = describeData(df)
+    assert isinstance(df, pd.DataFrame)
+    assert 'max' in descriptive_df.index
+    assert df.shape[0]==89
+    print(descriptive_df)
+    print(df)
+    plotData(df)
+test_func()
